0b5df8191d22a08e10c81ab906bc624070a48cf0
nienow <system@randombits.dev>
2023-06-27 16:52:19 -0500

    Reverted error handling

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,7 +1,7 @@
 import React, {useRef} from 'react';
 import {useEffect} from "preact/compat";
 import * as monaco from 'monaco-editor';
-import {getNoteText, isNoteLocked, updateNoteText} from "../sn-api";
+import {getNoteText, updateNoteText} from "../sn-api";
 import Examples from "./Examples";
 import {styled} from "goober";
 
@@ -22,7 +22,6 @@ const TextEditor = ({onUpdate}) => {
       language: 'mermaid',
       lineDecorationsWidth: 5,
       contextmenu: false,
-      readOnly: isNoteLocked(),
       value: getNoteText()
     };
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -60,9 +59,9 @@ const TextEditor = ({onUpdate}) => {
   return (
     <>
       <div ref={ref} id="editor" className="code-container"></div>
-      {isNoteLocked() ? <div>Editing Disabled</div> : <CodePanel>
+      <CodePanel>
         <Examples onSelect={selectExample}/>
-      </CodePanel>}
+      </CodePanel>
     </>
   );
 }
